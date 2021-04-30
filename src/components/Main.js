a262433af379fe658ed62dac890f93b1bf0738f7
sunlight-nadezhda <75030781+sunlight-nadezhda@users.noreply.github.com>
2021-04-30 17:04:01 +0300

    fix: remake click handlers

--- src/components/Main.js
+++ src/components/Main.js
@@ -1,37 +1,25 @@
 import React from "react";
 
 function Main() {
-    React.useEffect(() => {
-        function handleEditAvatarClick(event) {
-            document
-                .querySelector(".popup_type_edit-avatar")
-                .classList.add("popup_opened");
-        }
-        function handleEditProfileClick(event) {
-            document
-                .querySelector(".popup_type_profile")
-                .classList.add("popup_opened");
-        }
-        function handleAddPlaceClick(event) {
-            document
-                .querySelector(".popup_type_add-card")
-                .classList.add("popup_opened");
-        }
+    function handleEditAvatarClick(event) {
         document
-            .querySelector(".profile__avatar-edit")
-            .addEventListener("click", handleEditAvatarClick);
+            .querySelector(".popup_type_edit-avatar")
+            .classList.add("popup_opened");
+    }
+    function handleEditProfileClick(event) {
         document
-            .querySelector(".profile__edit-button")
-            .addEventListener("click", handleEditProfileClick);
+            .querySelector(".popup_type_profile")
+            .classList.add("popup_opened");
+    }
+    function handleAddPlaceClick(event) {
         document
-            .querySelector(".profile__add-button")
-            .addEventListener("click", handleAddPlaceClick);
-        return () => {};
-    });
+            .querySelector(".popup_type_add-card")
+            .classList.add("popup_opened");
+    }
     return (
         <main>
             <section className="profile">
-                <div className="profile__avatar-edit"></div>
+                <div className="profile__avatar-edit" onClick={handleEditAvatarClick}></div>
                 <div className="profile__about">
                     <h1 className="profile__name">Жак-Ив Кусто</h1>
                     <p className="profile__metier">Исследователь океана</p>
@@ -39,12 +27,14 @@ function Main() {
                         type="button"
                         aria-label="Редактировать"
                         className="button profile__edit-button"
+                        onClick={handleEditProfileClick}
                     ></button>
                 </div>
                 <button
                     type="button"
                     aria-label="Добавить"
                     className="button profile__add-button"
+                    onClick={handleAddPlaceClick}
                 ></button>
             </section>
 
