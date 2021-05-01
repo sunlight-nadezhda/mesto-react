d25676e1564e7798d898e81c18431c4a97a4f0c7
sunlight-nadezhda <75030781+sunlight-nadezhda@users.noreply.github.com>
2021-05-01 16:11:00 +0300

    feat: add cards and number likes

--- src/components/Main.js
+++ src/components/Main.js
@@ -1,13 +1,46 @@
 import React from "react";
+import api from "./../utils/api";
 
 function Main(props) {
+    const [userName, setUserName] = React.useState("");
+    const [userDescription, setUserDescription] = React.useState("");
+    const [userAvatar, setUserAvatar] = React.useState("");
+    const [cards, setCards] = React.useState([]);
+
+    React.useEffect(() => {
+        api.getUserInfo()
+            .then((userInfo) => {
+                setUserName(userInfo.name);
+                setUserAvatar(userInfo.avatar);
+                setUserDescription(userInfo.about);
+            })
+            .catch((err) => {
+                console.log(err);
+            });
+    }, []);
+
+    React.useEffect(() => {
+        api.getInitialCards()
+            .then((dataCard) => {
+                setCards(dataCard);
+                console.log(dataCard);
+            })
+            .catch((err) => {
+                console.log(err);
+            });
+    }, []);
+
     return (
         <main>
             <section className="profile">
-                <div className="profile__avatar-edit" onClick={props.onEditAvatar}></div>
+                <div
+                    className="profile__avatar-edit"
+                    onClick={props.onEditAvatar}
+                    style={{ backgroundImage: `url(${userAvatar})` }}
+                ></div>
                 <div className="profile__about">
-                    <h1 className="profile__name">Жак-Ив Кусто</h1>
-                    <p className="profile__metier">Исследователь океана</p>
+                    <h1 className="profile__name">{userName}</h1>
+                    <p className="profile__metier">{userDescription}</p>
                     <button
                         type="button"
                         aria-label="Редактировать"
@@ -24,7 +57,38 @@ function Main(props) {
             </section>
 
             <section aria-label="Карточки с местами">
-                <ul className="elements"></ul>
+                <ul className="elements">
+                {
+                    cards.map((card) => (
+                        <li className="element" key={card._id}>
+                            <img
+                                className="element__image"
+                                src={card.link}
+                                alt={card.name}
+                            />
+                            <button
+                                type="button"
+                                aria-label="Удалить"
+                                className="button element__trash"
+                            ></button>
+                            <div className="element__info">
+                                <h2 className="element__title">{card.name}</h2>
+                                <div className="element__likes">
+                                    <button
+                                        type="button"
+                                        aria-label="Лайкнуть"
+                                        className="button element__like-button"
+                                    ></button>
+                                    <span
+                                        className="element__like-counter"
+                                        aria-label="Количество лайков"
+                                    >{card.likes.length}</span>
+                                </div>
+                            </div>
+                        </li>
+                    ))
+                }
+                </ul>
             </section>
         </main>
     );
